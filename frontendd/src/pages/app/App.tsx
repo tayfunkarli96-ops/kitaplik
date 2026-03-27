1c869cb2fef5540efa77e65f625131230859567e
tayfun <tayfunkarli96@gmail.com>
2026-03-27 18:36:51 +0300

    Update App.tsx

--- frontendd/src/pages/app/App.tsx
+++ frontendd/src/pages/app/App.tsx
@@ -1,40 +1,41 @@
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-// Senin Navbar dosyanın yeri
+
+// Navbar yolu (Bunu zaten bulmuştuk)
 import Navbar from '../../components/app/Navbar'; 
 
-// Sayfa Importları (Klasör isimlerine göre harf harfine ayarladım)
+// Sayfa Importları
 import HomePage from '../home/HomePage';
 import MoviesPage from '../movies/MoviesPage';
 import NewsPage from '../news/NewsPage'; 
 import ProfilePage from '../profile/ProfilePage';
 import MovieDetailsPage from '../movies/MovieDetailsPage';
 import AboutPage from '../about/AboutPage';
-import ContactPage from '../contacts/ContactPage'; // Klasörün "contacts" idi
 import QuizPage from '../quiz/QuizPage';
+
+/** * DİKKAT: Hata buradaydı! 
+ * Eğer bu satır hata verirse (yani build yine kırmızı yanarsa), 
+ * dosyanın adını '../contacts/Contact' olarak değiştirmeyi dene.
+ */
+import ContactPage from '../contacts/ContactPage'; 
 
 function App() {
   return (
     <Router>
-      <div className="flex bg-black min-h-screen">
-        {/* Yan Menü (Navbar) */}
-        <Navbar />
+      {/* Orijinal Navbar'ın (CSS'ini bozmadan) */}
+      <Navbar />
 
-        {/* Ana İçerik Alanı */}
-        {/* NOT: ml-64 arayüzü bozuyorsa buradaki "ml-64" yazısını silebilirsin */}
-        <main className="flex-1 ml-64 p-4 text-white min-h-screen">
-          <Routes>
-            <Route path="/" element={<HomePage />} />
-            <Route path="/movies" element={<MoviesPage />} />
-            <Route path="/news" element={<NewsPage />} />
-            <Route path="/profile" element={<ProfilePage />} />
-            <Route path="/about" element={<AboutPage />} />
-            <Route path="/contact" element={<ContactPage />} />
-            <Route path="/quiz" element={<QuizPage />} />
-            <Route path="/movie/:id" element={<MovieDetailsPage />} />
-          </Routes>
-        </main>
-      </div>
+      {/* Sayfa Geçişleri (Arayüzü bozacak ek div'leri sildim) */}
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/movies" element={<MoviesPage />} />
+        <Route path="/news" element={<NewsPage />} />
+        <Route path="/profile" element={<ProfilePage />} />
+        <Route path="/about" element={<AboutPage />} />
+        <Route path="/contact" element={<ContactPage />} />
+        <Route path="/quiz" element={<QuizPage />} />
+        <Route path="/movie/:id" element={<MovieDetailsPage />} />
+      </Routes>
     </Router>
   );
 }
