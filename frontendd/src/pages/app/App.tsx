a13f3a991732c8dd23958c73a50692578c69d6af
tayfun <tayfunkarli96@gmail.com>
2026-03-27 18:07:39 +0300

    Update App.tsx

--- frontendd/src/pages/app/App.tsx
+++ frontendd/src/pages/app/App.tsx
@@ -1,70 +1,43 @@
+import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import './App.css';
-import Navbar from '@components/app/Navbar';
-import HomePage from '@pages/home/HomePage';
-import MovieSearch from '@pages/movies/MovieSearchPage';
-import ErrorPage from '@pages/error/ErrorPage';
-import LoginRegisterPage from '@pages/login_register/LoginRegisterPage';
-import MovieDetailsPage from '@pages/movies/MovieDetailsPage';
-import NewsDetailsPage from '@pages/news/NewsDetailsPage';
-import NewsPage from '@pages/news/NewsPage';
-import ProfilePage from '@pages/profile/ProfilePage';
-import QuizPage from '@pages/quiz/QuizPage';
-import RecPage from '@pages/rec/RecPage';
-import AboutPage from '@pages/about/AboutPage';
-import ContactsPage from '@pages/contacts/ContactsPage';
-import PersonDetailsPage from '@src/pages/person/PersonDetailsPage';
-import MoviesPage from '@pages/movies/MoviesPage';
-import { AuthProvider, useAuth } from '@src/context/AuthContext';
-import ProtectedRoute from '@components/app/ProtectedRoute';
+import Navbar from './components/Sidebar'; // Sidebar dosyanın adıyla aynı olduğundan emin ol
+import HomePage from './pages/home/HomePage';
+import MoviesPage from './pages/movies/MoviesPage';
+import NewsPage from './pages/news/NewsPage'; // İşte o yeni haberler sayfan
+import ProfilePage from './pages/profile/ProfilePage';
+import MovieDetailsPage from './pages/movies/MovieDetailsPage';
 
-const AppContent = () => {
-  const { isLoggedIn, isLoading } = useAuth();
-
-  if (isLoading) {
-    return <div className="app-loading">Loading...</div>;
-  }
-
-  return (
-    <div className="app">
-      <Navbar />
-      <Routes>
-        <Route path="/" element={<HomePage />} />
-        <Route path="/search" element={<MovieSearch />} />
-        <Route path="/movies" element={<MoviesPage />} />
-        <Route path="/recs" element={<RecPage />} />
-        <Route path="/movies/:movieId" element={<MovieDetailsPage />} />
-        <Route path="/person/:identifier" element={<PersonDetailsPage />} />
-        <Route path="/news" element={<NewsPage />} />
-        <Route path="/news/:newsId" element={<NewsDetailsPage />} />
-        <Route path="/newsd" element={<NewsDetailsPage />} />
-        <Route path="/about" element={<AboutPage />} />
-        <Route path="/contacts" element={<ContactsPage />} />
-        <Route path="/login" element={isLoggedIn ? <HomePage /> : <LoginRegisterPage />} />
-        <Route path="/quiz" element={
-          <ProtectedRoute>
-            <QuizPage />
-          </ProtectedRoute>
-        } />
-        <Route path="/profile" element={
-          <ProtectedRoute>
-            <ProfilePage />
-          </ProtectedRoute>
-        } />
-        <Route path="*" element={<ErrorPage />} />
-      </Routes>
-    </div>
-  );
-};
-
-const App = () => {
+function App() {
   return (
     <Router>
-      <AuthProvider>
-        <AppContent />
-      </AuthProvider>
+      <div className="flex bg-black min-h-screen">
+        {/* Sol taraftaki menü (Sidebar) */}
+        <Navbar />
+
+        {/* Sağ taraftaki asıl içerik alanı */}
+        <main className="flex-1 ml-64 p-4 text-white">
+          <Routes>
+            {/* Ana Sayfa */}
+            <Route path="/" element={<HomePage />} />
+
+            {/* Film Listesi */}
+            <Route path="/movies" element={<MoviesPage />} />
+
+            {/* RE-09: Haberler Sayfası */}
+            <Route path="/news" element={<NewsPage />} />
+
+            {/* RE-01: Profil Sayfası */}
+            <Route path="/profile" element={<ProfilePage />} />
+
+            {/* RE-02/03/10: Film Detay Sayfası */}
+            <Route path="/movie/:id" element={<MovieDetailsPage />} />
+            
+            {/* Eğer başka sayfaların varsa buraya ekleyebilirsin */}
+          </Routes>
+        </main>
+      </div>
     </Router>
   );
-};
+}
 
 export default App;
