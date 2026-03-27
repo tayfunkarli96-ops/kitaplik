85c88b76563366282f51326327182afb920bf989
tayfun <tayfunkarli96@gmail.com>
2026-03-27 19:24:59 +0300

    Create AddMovie.tsx

---
+++ frontendd/src/pages/movies/nested/AddMovie.tsx
@@ -0,0 +1,3 @@
+import React from 'react';
+const AddMovie = () => <div className="p-8 text-pink-400 font-bold text-2xl bg-gray-950 rounded-2xl border border-gray-800">➕ Film Ekleme Paneli (Yapım Aşamasında)</div>;
+export default AddMovie;
