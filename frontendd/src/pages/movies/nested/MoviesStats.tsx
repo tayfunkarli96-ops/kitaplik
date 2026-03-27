76d6db33c8d900275db957c3b338e358162a8d5a
tayfun <tayfunkarli96@gmail.com>
2026-03-27 19:28:27 +0300

    Delete frontendd/src/pages/movies/nested directory

--- frontendd/src/pages/movies/nested/MoviesStats.tsx
+++
@@ -1,3 +0,0 @@
-import React from 'react';
-const MoviesStats = () => <div className="p-8 text-cyan-400 font-bold text-2xl bg-gray-950 rounded-2xl border border-gray-800">📊 İstatistikler Paneli (Yapım Aşamasında)</div>;
-export default MoviesStats;
