50e46ebcc2bd3931d3f556dfeaef9f2748fc06d9
tayfun <tayfunkarli96@gmail.com>
2026-03-27 19:25:36 +0300

    Create MoviesStats.tsx

---
+++ frontendd/src/pages/movies/nested/MoviesStats.tsx
@@ -0,0 +1,3 @@
+import React from 'react';
+const MoviesStats = () => <div className="p-8 text-cyan-400 font-bold text-2xl bg-gray-950 rounded-2xl border border-gray-800">📊 İstatistikler Paneli (Yapım Aşamasında)</div>;
+export default MoviesStats;
