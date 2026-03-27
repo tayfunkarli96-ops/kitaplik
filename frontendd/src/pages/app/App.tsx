8b124c776cb740edf42b6d2296e8b330796cc37a
tayfun <tayfunkarli96@gmail.com>
2026-03-27 23:50:55 +0300

    Update App.tsx

--- frontendd/src/pages/app/App.tsx
+++ frontendd/src/pages/app/App.tsx
@@ -2,7 +2,7 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Navbar from '../../components/app/Navbar'; 
 import HomePage from '../home/HomePage';
 import NewsPage from '../news/NewsPage'; 
-import ProfilePage from '../profile/ProfilePage';
+import ProfilePage from '../profile/ProfilePage'; // Üye girişi için bunu kullanacağız
 import AboutPage from '../about/AboutPage';
 import QuizPage from '../quiz/QuizPage';
 import MovieDetailsPage from '../movies/MovieDetailsPage';
@@ -16,11 +16,15 @@ function App() {
         <Routes>
           <Route path="/" element={<HomePage />} />
           <Route path="/news" element={<NewsPage />} />
-          <Route path="/profile" element={<ProfilePage />} />
           <Route path="/about" element={<AboutPage />} />
           <Route path="/quiz" element={<QuizPage />} />
           <Route path="/movie/:id" element={<MovieDetailsPage />} />
           <Route path="/movies" element={<MoviesPage />} />
+          
+          {/* ÜYE GİRİŞİ İÇİN YOL EKLENDİ */}
+          {/* Şu an login sayfan olmadığı için profil sayfasına yönlendiriyoruz */}
+          <Route path="/login" element={<ProfilePage />} />
+          <Route path="/profile" element={<ProfilePage />} />
         </Routes>
       </div>
     </Router>
