3e8f2fdad332133006e0fd5e35af5bb769a7000f
tayfun <tayfunkarli96@gmail.com>
2026-03-27 20:25:22 +0300

    Update App.tsx

--- frontendd/src/pages/app/App.tsx
+++ frontendd/src/pages/app/App.tsx
@@ -1,32 +1,33 @@
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 
-import Navbar from '../../components/app/Navbar'; 
-import HomePage from '../home/HomePage';
-import NewsPage from '../news/NewsPage'; 
-import ProfilePage from '../profile/ProfilePage';
-import AboutPage from '../about/AboutPage';
-import QuizPage from '../quiz/QuizPage';
-import MovieDetailsPage from '../movies/MovieDetailsPage';
-import MoviesPage from '../movies/MoviesPage'; 
-
-// İŞTE BURASI: ContactsPage (s harfi eklendi, Vercel artık çökmeyecek!)
-import ContactPage from '../contacts/ContactsPage'; 
+// YOLLAR DÜZELTİLDİ: Artık Vercel hata vermeyecek!
+import Navbar from './components/app/Navbar'; 
+import HomePage from './pages/home/HomePage';
+import NewsPage from './pages/news/NewsPage'; 
+import ProfilePage from './pages/profile/ProfilePage';
+import AboutPage from './pages/about/AboutPage';
+import QuizPage from './pages/quiz/QuizPage';
+import MovieDetailsPage from './pages/movies/MovieDetailsPage';
+import MoviesPage from './pages/movies/MoviesPage'; 
+import ContactPage from './pages/contacts/ContactsPage'; 
 
 function App() {
   return (
     <Router>
       <Navbar />
-      <Routes>
-        <Route path="/" element={<HomePage />} />
-        <Route path="/news" element={<NewsPage />} />
-        <Route path="/profile" element={<ProfilePage />} />
-        <Route path="/about" element={<AboutPage />} />
-        <Route path="/quiz" element={<QuizPage />} />
-        <Route path="/contact" element={<ContactPage />} /> {/* İletişim sayfasını geri ekledim */}
-        <Route path="/movie/:id" element={<MovieDetailsPage />} />
-        <Route path="/movies" element={<MoviesPage />} />
-      </Routes>
+      <div style={{ paddingTop: '60px' }}> {/* Navbar'ın altında kalmasın diye */}
+        <Routes>
+          <Route path="/" element={<HomePage />} />
+          <Route path="/news" element={<NewsPage />} />
+          <Route path="/profile" element={<ProfilePage />} />
+          <Route path="/about" element={<AboutPage />} />
+          <Route path="/quiz" element={<QuizPage />} />
+          <Route path="/contact" element={<ContactPage />} />
+          <Route path="/movie/:id" element={<MovieDetailsPage />} />
+          <Route path="/movies" element={<MoviesPage />} />
+        </Routes>
+      </div>
     </Router>
   );
 }
