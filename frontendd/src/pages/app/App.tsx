58696554ef2514052b6c11a876d4e5b6c69e85ff
tayfun <tayfunkarli96@gmail.com>
2026-03-28 02:52:45 +0300

    Update App.tsx

--- frontendd/src/pages/app/App.tsx
+++ frontendd/src/pages/app/App.tsx
@@ -3,8 +3,6 @@ import Navbar from './components/app/Navbar';
 import HomePage from './pages/home/HomePage';
 import MoviesPage from './pages/movies/MoviesPage'; 
 import ContactsPage from './pages/contacts/ContactsPage'; 
-import NewsPage from './pages/news/NewsPage'; 
-import AboutPage from './pages/about/AboutPage';
 
 function App() {
   return (
@@ -14,9 +12,8 @@ function App() {
         <Routes>
           <Route path="/" element={<HomePage />} />
           <Route path="/movies" element={<MoviesPage />} />
-          <Route path="/news" element={<NewsPage />} />
-          <Route path="/about" element={<AboutPage />} />
           <Route path="/contacts" element={<ContactsPage />} />
+          {/* Diğer sayfaların varsa buraya ekle */}
         </Routes>
       </div>
     </Router>
