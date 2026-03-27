3c2c44958a5eaabe976d458c02ed99a3adb99641
tayfun <tayfunkarli96@gmail.com>
2026-03-28 02:35:49 +0300

    Update App.tsx

--- frontendd/src/pages/app/App.tsx
+++ frontendd/src/pages/app/App.tsx
@@ -1,4 +1,4 @@
-import { HashRouter as Router, Routes, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Navbar from './components/app/Navbar'; 
 import HomePage from './pages/home/HomePage';
 import NewsPage from './pages/news/NewsPage'; 
@@ -7,7 +7,6 @@ import AboutPage from './pages/about/AboutPage';
 import QuizPage from './pages/quiz/QuizPage';
 import MovieDetailsPage from './pages/movies/MovieDetailsPage';
 import MoviesPage from './pages/movies/MoviesPage'; 
-// --- YENİ EKLENEN KAPI ---
 import ContactsPage from './pages/contacts/ContactsPage'; 
 
 function App() {
@@ -17,15 +16,13 @@ function App() {
       <div style={{ paddingTop: '60px' }}>
         <Routes>
           <Route path="/" element={<HomePage />} />
+          <Route path="/movies" element={<MoviesPage />} />
           <Route path="/news" element={<NewsPage />} />
           <Route path="/about" element={<AboutPage />} />
           <Route path="/quiz" element={<QuizPage />} />
-          <Route path="/movie/:id" element={<MovieDetailsPage />} />
-          <Route path="/movies" element={<MoviesPage />} />
-          <Route path="/login" element={<ProfilePage />} />
-          <Route path="/profile" element={<ProfilePage />} />
-          {/* --- İŞTE BU KADAR, SİSTEMİ BOZMAZ --- */}
           <Route path="/contacts" element={<ContactsPage />} />
+          <Route path="/movie/:id" element={<MovieDetailsPage />} />
+          <Route path="/profile" element={<ProfilePage />} />
         </Routes>
       </div>
     </Router>
