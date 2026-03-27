6aa405ed5fc9e241a22b7b2147635ccca831b967
tayfun <tayfunkarli96@gmail.com>
2026-03-27 19:29:25 +0300

    Update App.tsx

--- frontendd/src/pages/app/App.tsx
+++ frontendd/src/pages/app/App.tsx
@@ -1,6 +1,6 @@
 
-  import React from 'react';
-import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 
 import Navbar from '../../components/app/Navbar'; 
 import HomePage from '../home/HomePage';
@@ -9,12 +9,7 @@ import ProfilePage from '../profile/ProfilePage';
 import AboutPage from '../about/AboutPage';
 import QuizPage from '../quiz/QuizPage';
 import MovieDetailsPage from '../movies/MovieDetailsPage';
-
-// FİLM YÖNETİMİ DOSYALARI
 import MoviesPage from '../movies/MoviesPage'; 
-import MoviesManagement from '../movies/nested/MoviesManagement'; 
-import AddMovie from '../movies/nested/AddMovie'; 
-import MoviesStats from '../movies/nested/MoviesStats'; 
 
 function App() {
   return (
@@ -26,15 +21,10 @@ function App() {
         <Route path="/profile" element={<ProfilePage />} />
         <Route path="/about" element={<AboutPage />} />
         <Route path="/quiz" element={<QuizPage />} />
+        {/* Film Detay Rotası */}
         <Route path="/movie/:id" element={<MovieDetailsPage />} />
-
-        {/* İÇ İÇE ROTALAR (SOL MENÜ İÇİN) */}
-        <Route path="/movies" element={<MoviesPage />}>
-          <Route index element={<Navigate to="management" replace />} /> 
-          <Route path="management" element={<MoviesManagement />} />
-          <Route path="add" element={<AddMovie />} />
-          <Route path="stats" element={<MoviesStats />} />
-        </Route>
+        {/* Film Listesi Rotası */}
+        <Route path="/movies" element={<MoviesPage />} />
       </Routes>
     </Router>
   );
