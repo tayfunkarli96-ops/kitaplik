f8bae56fa70ae43cc7e5dfc72846a3d43fde3503
tayfun <tayfunkarli96@gmail.com>
2026-03-28 14:00:33 +0300

    Update App.tsx

--- frontendd/src/pages/app/App.tsx
+++ frontendd/src/pages/app/App.tsx
@@ -1,19 +1,21 @@
-
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { HashRouter as Router, Routes, Route } from 'react-router-dom';
 import Navbar from './components/app/Navbar'; 
 import HomePage from './pages/home/HomePage';
 import MoviesPage from './pages/movies/MoviesPage'; 
 import ContactsPage from './pages/contacts/ContactsPage'; 
+// Varsa NewsPage, AboutPage falan onları da buraya import et
 
 function App() {
   return (
     <Router>
       <Navbar />
-      <div style={{ paddingTop: '60px' }}>
+      <div style={{ paddingTop: '80px' }}>
         <Routes>
           <Route path="/" element={<HomePage />} />
           <Route path="/movies" element={<MoviesPage />} />
+          {/* İŞTE İLETİŞİM SAYFASININ KİLİDİ BURASI */}
           <Route path="/contacts" element={<ContactsPage />} />
+          {/* Diğer Route'larını da buraya ekle */}
         </Routes>
       </div>
     </Router>
