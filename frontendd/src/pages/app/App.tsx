0a0c808252b5b594643050f630b831991a1c4eea
tayfun <tayfunkarli96@gmail.com>
2026-05-24 21:52:07 +0300

    Update App.tsx

--- frontendd/src/pages/app/App.tsx
+++ frontendd/src/pages/app/App.tsx
@@ -1,25 +1,11 @@
-import { HashRouter as Router, Routes, Route } from 'react-router-dom';
-
-// 🚨 YOLLAR DÜZELTİLDİ! Senin dosya yapına özel:
-import Navbar from '../../components/app/Navbar'; 
-import HomePage from '../home/HomePage';
-import MoviesPage from '../movies/MoviesPage'; 
-import ContactsPage from '../contacts/ContactsPage'; 
-// (Eğer NewsPage falan varsa onları da "../news/NewsPage" diye ekle)
+import React from 'react';
+import MovieList from '../../components/MovieList'; // Dosyanın yerini (yolunu) buna göre güncelledim
 
 function App() {
   return (
-    <Router>
-      <Navbar />
-      <div style={{ paddingTop: '80px' }}>
-        <Routes>
-          <Route path="/" element={<HomePage />} />
-          <Route path="/movies" element={<MoviesPage />} />
-          <Route path="/contacts" element={<ContactsPage />} />
-          {/* Varsa diğer rotaların */}
-        </Routes>
-      </div>
-    </Router>
+    <div className="App">
+      <MovieList />
+    </div>
   );
 }
 
