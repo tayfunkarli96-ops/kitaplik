5f341af1380dd8cfa5f0fbaa82e3fe079a3c2883
tayfun <tayfunkarli96@gmail.com>
2026-05-24 21:48:14 +0300

    Update MovieList.tsx

--- frontendd/src/components/MovieList.tsx
+++ frontendd/src/components/MovieList.tsx
@@ -1,1 +1,13 @@
 
+import React from 'react';
+
+const MovieList = () => {
+  return (
+    <div style={{ padding: '20px' }}>
+      <h1>Cornflix Film Listesi</h1>
+      <p>Bu ekran mobil uygulama gereksinimi için oluşturulmuştur.</p>
+    </div>
+  );
+};
+
+export default MovieList;
