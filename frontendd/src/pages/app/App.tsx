efa93277dc6367057b1dc59e98a81762de1cb13f
tayfun <tayfunkarli96@gmail.com>
2026-05-24 21:59:07 +0300

    Update App.tsx

--- frontendd/src/pages/app/App.tsx
+++ frontendd/src/pages/app/App.tsx
@@ -1,10 +1,25 @@
 import React from 'react';
-import MovieList from '../../components/MovieList'; // Dosyanın yerini (yolunu) buna göre güncelledim
+import Header from '../../components/Header';
+import MovieList from '../../components/MovieList';
 
 function App() {
   return (
-    <div className="App">
-      <MovieList />
+    <div style={{ 
+      backgroundColor: '#141414', // Cornflix koyu arka planı
+      minHeight: '100vh', // Ekranın tamamını kaplaması için
+      fontFamily: "'Helvetica Neue', Helvetica, Arial, sans-serif"
+    }}>
+      {/* Üst Kırmızı Menü */}
+      <Header />
+      
+      {/* İçerik Alanı (Mobil ekrana tam oturması için ortalandı) */}
+      <div style={{ 
+        maxWidth: '500px', // Telefon ekranı genişliği simülasyonu
+        margin: '0 auto', // Ortalamak için
+        padding: '15px'
+      }}>
+        <MovieList />
+      </div>
     </div>
   );
 }
